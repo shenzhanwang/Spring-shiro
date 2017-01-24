91cdc978f11a6550178476883f1b1043507b18a5
shenzhanwang <765410342@qq.com>
2017-01-24 14:31:55 +0800

    2

---
+++ shiro.sql
@@ -0,0 +1,154 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: shiro
+-- ------------------------------------------------------
+-- Server version	5.7.13-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sys_permissions`
+--
+
+DROP TABLE IF EXISTS `sys_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_permissions` (
+  `permissionid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `permission` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`permissionid`),
+  UNIQUE KEY `idx_sys_permissions_permission` (`permission`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_permissions`
+--
+
+LOCK TABLES `sys_permissions` WRITE;
+/*!40000 ALTER TABLE `sys_permissions` DISABLE KEYS */;
+INSERT INTO `sys_permissions` VALUES (1,'user:create','用户模块新增'),(2,'user:update','用户模块修改'),(3,'menu:create','菜单模块新增');
+/*!40000 ALTER TABLE `sys_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_roles`
+--
+
+DROP TABLE IF EXISTS `sys_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_roles` (
+  `roleid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`roleid`),
+  UNIQUE KEY `idx_sys_roles_role` (`role`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_roles`
+--
+
+LOCK TABLES `sys_roles` WRITE;
+/*!40000 ALTER TABLE `sys_roles` DISABLE KEYS */;
+INSERT INTO `sys_roles` VALUES (1,'admin','管理员'),(2,'user','用户管理员');
+/*!40000 ALTER TABLE `sys_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_roles_permissions`
+--
+
+DROP TABLE IF EXISTS `sys_roles_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_roles_permissions` (
+  `role_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`role_id`,`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_roles_permissions`
+--
+
+LOCK TABLES `sys_roles_permissions` WRITE;
+/*!40000 ALTER TABLE `sys_roles_permissions` DISABLE KEYS */;
+INSERT INTO `sys_roles_permissions` VALUES (1,1),(1,2),(1,3),(2,1),(2,2);
+/*!40000 ALTER TABLE `sys_roles_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_users`
+--
+
+DROP TABLE IF EXISTS `sys_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `locked` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_sys_users_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_users`
+--
+
+LOCK TABLES `sys_users` WRITE;
+/*!40000 ALTER TABLE `sys_users` DISABLE KEYS */;
+INSERT INTO `sys_users` VALUES (2,'zhang','$shiro1$SHA-256$500000$FntKPgqBpEOqVkCyP9OY0A==$V3bBPA0gNp44r6gQ8Mm4kf5wgrVvXLn0MLEsZkyb3RU=',0),(6,'ddd','$shiro1$SHA-256$500000$FntKPgqBpEOqVkCyP9OY0A==$V3bBPA0gNp44r6gQ8Mm4kf5wgrVvXLn0MLEsZkyb3RU=',0);
+/*!40000 ALTER TABLE `sys_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_users_roles`
+--
+
+DROP TABLE IF EXISTS `sys_users_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_users_roles` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_users_roles`
+--
+
+LOCK TABLES `sys_users_roles` WRITE;
+/*!40000 ALTER TABLE `sys_users_roles` DISABLE KEYS */;
+INSERT INTO `sys_users_roles` VALUES (2,1);
+/*!40000 ALTER TABLE `sys_users_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-24 14:31:25
